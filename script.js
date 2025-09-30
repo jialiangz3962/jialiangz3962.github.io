1f4402402c4368b69afc56ab6f3c59f6e0fe15ce
jialiangz3962 <jialiangz2@nycstudents.net>
2025-09-30 09:25:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jia Liang Zhong"; // update the part BETWEEN the "quotes"
+let userName = "jialiangz3962"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
